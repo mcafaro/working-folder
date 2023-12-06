572b9f377d6d80a4da5be962f91a457381f379d4
Mark Cafaro <34887852+mcafaro@users.noreply.github.com>
2023-12-06 17:10:34 -0500

    Create mytest.m

---
+++ subfolder/mytest.m
@@ -0,0 +1,19 @@
+classdef mytest < matlab.unittest.TestCase
+
+    methods (TestClassSetup)
+        % Shared setup for the entire test class
+    end
+
+    methods (TestMethodSetup)
+        % Setup for each test
+    end
+
+    methods (Test)
+        % Test methods
+
+        function test1(testCase)
+            pwd
+        end
+    end
+
+end
